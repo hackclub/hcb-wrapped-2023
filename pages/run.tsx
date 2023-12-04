82ef4d9da539b51300130c944d54d3f2e4322cce
Sam Poder <sam.r.poder@gmail.com>
2023-12-04 09:42:17 -0800

    `test.json`!

--- pages/run.tsx
+++ pages/run.tsx
@@ -1,6 +1,8 @@
 import { Wrapped } from "@/components/Wrapped";
 import { generateTestData } from "@/components/Wrapped/utils/data";
 import type { WrappedData } from "@/components/Wrapped/utils/data";
+import fs from 'fs/promises';
+import path from 'path';
 
 import dynamic from "next/dynamic";
 
@@ -15,9 +17,23 @@ export default function Run({ data }: { data: WrappedData }) {
 // prevents hydration errors
 
 export async function getServerSideProps() {
-  return {
-    props: {
-      data: generateTestData()
-    }
-  };
+  try {
+    // Attempt to load data from data.json
+    const filePath = path.join(process.cwd(), 'test.json');
+    const jsonData = await fs.readFile(filePath, 'utf-8');
+    const data = JSON.parse(jsonData);
+    return {
+      props: {
+        data
+      }
+    };
+  } catch (error) {
+    // Default to test data
+    return {
+      props: {
+        data: generateTestData()
+      }
+    };
+  }
 }
+
