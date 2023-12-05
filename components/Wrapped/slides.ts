6eb9400500b125f5e4f147c8499e842c853d8ea3
Aquoria <134425658+Aquoria@users.noreply.github.com>
2023-12-05 13:10:47 -0500

    Revert "Added Word Cloud Slide"

--- components/Wrapped/slides.ts
+++ components/Wrapped/slides.ts
@@ -1,6 +1,5 @@
 import HCB from "./slides/HCB";
 import HelloWorld from "./slides/HelloWorld";
 import Start from "./slides/Start";
-import WordCloud from "./slides/WordCloud";
 
-export default [HelloWorld, Start, HCB, WordCloud];
+export default [HelloWorld, Start, HCB];
