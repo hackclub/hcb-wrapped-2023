7c85e02c8961a78af25309382ef8e9c18e78ff94
Ivoine <ivoinestrachan242@gmail.com>
2023-12-05 10:31:51 -0500

    implement most money spent by location

--- components/Wrapped/slides.ts
+++ components/Wrapped/slides.ts
@@ -1,5 +1,6 @@
 import HCB from "./slides/HCB";
 import HelloWorld from "./slides/HelloWorld";
 import Start from "./slides/Start";
+import MoneySpent from "./slides/MoneySpent"
 
-export default [HelloWorld, Start, HCB];
+export default [HelloWorld, Start, HCB, MoneySpent];
---
+++ components/Wrapped/slides/MoneySpent.tsx
@@ -0,0 +1,56 @@
+import $ from "@/utils/theme";
+import type { SlideProps, SlideOptions } from "../internals/slidesHelper";
+import { USDollarNoCents } from "../utils/formatter";
+import Background from "../components/Background";
+
+type SpendingData = [string, number];
+
+export default function TemplateSlide({ data }: SlideProps) {
+  const sortBySpending = (entries: SpendingData[]): SpendingData[] => {
+    return entries.sort((a: SpendingData, b: SpendingData) => b[1] - a[1]);
+  };
+
+  const SpendingByLocation = sortBySpending(
+    Object.entries(data.hcb.spendingByLocation) as SpendingData[]
+  ).slice(0, 2);
+
+  const SpendingByMerchant = sortBySpending(
+    Object.entries(data.hcb.spendingByMerchant) as SpendingData[]
+  ).slice(0, 2);
+
+  return (
+    <>
+      <h1
+        {...$.title({
+          textTransform: "uppercase"
+        })}
+      >
+        Money Spent
+      </h1>
+
+      <div>
+        {SpendingByLocation.map(([location, spending]: SpendingData) => (
+          <p key={location}>{`${location} - ${USDollarNoCents.format(
+            spending
+          )}`}</p>
+        ))}
+      </div>
+
+      <div>
+        {SpendingByMerchant.map(([merchant, spending]: SpendingData) => (
+          <p key={merchant}>{`${merchant} - ${USDollarNoCents.format(
+            spending
+          )}`}</p>
+        ))}
+      </div>
+
+      <Background />
+    </>
+  );
+}
+
+TemplateSlide.config = {
+  bg: $.red,
+  duration: 5_000, // 5 seconds
+  skipSlide: () => false
+};
