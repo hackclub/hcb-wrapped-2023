db73d4703ea140f648475c9f7bef5b144ca3f118
Ivoine <ivoinestrachan242@gmail.com>
2023-12-05 17:55:08 -0500

    seperated file and fixed deployment bugged

--- components/Wrapped/slides.ts
+++ components/Wrapped/slides.ts
@@ -3,5 +3,6 @@ import HelloWorld from "./slides/HelloWorld";
 import Start from "./slides/Start";
 import MoneySpent from "./slides/MoneySpent"
 import OrgStats from "./slides/OrgStats"
+import Location from "./slides/LocationSpent"
 
-export default [HelloWorld, Start, HCB, MoneySpent, OrgStats];
+export default [HelloWorld, Start, HCB, MoneySpent, Location, OrgStats];
--- components/Wrapped/slides/LocationSpent.tsx
+++ components/Wrapped/slides/LocationSpent.tsx
@@ -1,37 +1,28 @@
 import $ from "@/utils/theme";
+//
 import type { SlideProps, SlideOptions } from "../internals/slidesHelper";
 import { USDollarNoCents } from "../utils/formatter";
 import Background from "../components/Background";
 
 type SpendingData = [string, number];
-
 export default function TemplateSlide({ data }: SlideProps) {
   const sortBySpending = (entries: SpendingData[]): SpendingData[] => {
     return entries.sort((a: SpendingData, b: SpendingData) => b[1] - a[1]);
   };
-
-  const SpendingByMerchant = sortBySpending(
-    Object.entries(data.hcb.spendingByMerchant) as SpendingData[]
+  const SpendingByLocation = sortBySpending(
+    Object.entries(data.hcb.spendingByLocation) as SpendingData[]
   ).slice(0, 2);
-
   return (
     <>
-      <h1
-        {...$.title({
-          textTransform: "uppercase"
-        })}
-      >
-        Money Spent
-      </h1>
+      <h1 {...$.title({})}>Location Spent</h1>
 
       <div>
-        {SpendingByMerchant.map(([merchant, spending]: SpendingData) => (
-          <p key={merchant}>{`${merchant} - ${USDollarNoCents.format(
+        {SpendingByLocation.map(([location, spending]: SpendingData) => (
+          <p key={location}>{`${location} - ${USDollarNoCents.format(
             spending / 100
           )}`}</p>
         ))}
       </div>
-
       <Background />
     </>
   );
@@ -41,4 +32,4 @@ TemplateSlide.config = {
   bg: $.red,
   duration: 5_000, // 5 seconds
   skipSlide: () => false
-};
+} satisfies SlideOptions;
