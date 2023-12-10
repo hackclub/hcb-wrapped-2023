3760fb2d23f59f0abbed03499d911a04649c58cc
Yoda <yoda@yodacode.xyz>
2023-12-09 19:54:44 -0800

    changes

---
+++ components/Wrapped/slides/Ending.tsx
@@ -0,0 +1,36 @@
+import $ from "@/utils/theme";
+import type { SlideProps, SlideOptions } from "../internals/slidesHelper";
+import Background from "../components/Background";
+import { USDollarNoCents } from "../utils/formatter";
+import HCBStat from "../components/HCBStat";
+
+export default function Ending({ data }: SlideProps) {
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100%",
+          paddingBottom: "30px"
+        }}
+      >
+        <h1
+          {...$.headline({ fontSize: "2em", marginTop: "0px", color: "white" })}
+        >
+          That was fun.
+        </h1>
+        <p>
+            See you again next year?
+        </p>
+        <Background />
+      </div>
+    </>
+  );
+}
+
+Ending.config = {
+  bgImage: `linear-gradient(rgba(37,36,41,0.5) 0%, rgba(37,36,41,0.85) 75%), url(https://cloud-e4hjosvw9-hack-club-bot.vercel.app/2outernet-110.jpeg)`
+} satisfies SlideOptions;
