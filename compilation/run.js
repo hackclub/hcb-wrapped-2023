2e60a9345830235c3c285da29bf861af73e84a14
Yoda <yoda@yodacode.xyz>
2023-12-15 22:40:05 -0800

    changes

---
+++ compilation/run.js
@@ -0,0 +1,10 @@
+import { exec } from "child-process-promise";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const $ = async commands => (await exec(commands.join(" "))).stdout;
+const _ = (...paths) => paths.join(dirname(fileURLToPath(import.meta.url)), ...paths);
+
+
+
+$`cd ${_('../')}`
