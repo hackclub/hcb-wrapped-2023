0a7b07e5be905506114740e1450f1fd6e0ac6fdb
Gary Tou <gary@garytou.com>
2023-12-17 03:01:01 -0800

    We're on a first name basis!

--- components/Wrapped/slides/Start.tsx
+++ components/Wrapped/slides/Start.tsx
@@ -93,6 +93,8 @@ export default function Start({ data, isPaused }: SlideProps) {
 
   const test = useTimedValue((x) => x, 0 as number, 500, !!isPaused);
 
+  const firstName = data.individual.name.split(" ")[0]
+
   return (
     <div
       style={{
@@ -120,7 +122,7 @@ export default function Start({ data, isPaused }: SlideProps) {
           marginBottom: $.s1
         })}
       >
-        Welcome {data.individual.name}; 2023 was a big year on HCB for you.
+        Welcome {firstName}; 2023 was a big year on HCB for you.
         <b>
           You spent over{" "}
           {USDollarNoCents.format(
